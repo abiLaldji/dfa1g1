a844df347f040d3921afee17585c408329fed0ff
abi <abi@XPS15>
2018-04-24 10:59:32 +0200

    Phase 3

---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,26 @@
+package jpu2016.gameframe;
+
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
+public class GameFrame {
+	
+	IEventPerformer eventPerfomer = new IEventPerformer();
+	
+	public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicBuilder, Observable observable) {
+		
+	}
+	
+	public void keyPressed(KeyEvent keyEvent) {
+		
+	}
+	
+	public void keyReleased(KeyEvent keyEvent) {
+		
+	}
+	
+	public void keyTyped(KeyEvent keyEvent) {
+		
+	}
+
+}
