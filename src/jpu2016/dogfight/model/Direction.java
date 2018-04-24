1b974a681d8e76a144fb6b689daec841139b6c28
Guillaume <Guillaume@LAPTOP-C6K5LSCU>
2018-04-24 11:25:00 +0200

    Phase 3

--- src/jpu2016/dogfight/model/Direction.java
+++ src/jpu2016/dogfight/model/Direction.java
@@ -1,5 +1,13 @@
 package jpu2016.dogfight.model;
 
 public enum Direction {
+	
+	UP,
+	
+	RIGHT, 
+	
+	DOWN, 
+	
+	LEFT;
 
 }
---
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -0,0 +1,98 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Point;
+
+
+public class Mobile {
+	
+	private int speed;
+	
+	;
+	
+	public Mobile (Direction direction, Position position, Dimension dimension, int speed, String image) {
+		
+	}
+	
+	public Direction getDirection() {
+		return new Direction;
+	}
+	
+	public void setDirection(Direction direction) {
+			
+	}
+	
+	public Point getPosition() {
+		return new Point;
+	}
+	
+	public Dimension getDimension() {
+		return new Dimension(1,1);
+	}
+	
+	public int getSpeed() {
+		return 1;
+	}
+	
+	public int getWidth() {
+		return 1;
+	}
+	
+	public int getHeight() {
+		return 1;
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void placeInArea(IArea area) {
+		
+	}
+	
+	public boolean isPlayer(int player) {
+		return true;
+	}
+	
+	private void moveUp() {
+		
+	}
+	
+	private void moveRight() {
+		
+	}
+	
+	private void moveDown() {
+		
+	}
+	
+	private void moveLeft() {
+		
+	}
+	
+	public Color getColor() {
+		return new Color;
+	}
+	
+	public IDogfightModel getIDogfightModel() {
+		return new IDogfightModel;
+	}
+	
+	public void setIDogfightModel(IDogfightModel dogfightModel) {
+		
+	}
+	
+	public boolean hit() {
+		return true;
+	}
+	
+	public boolean isWeapon() {
+		return true;
+	}
+	
+	public Image getImage() {
+		return Image;
+	}
+}
+
