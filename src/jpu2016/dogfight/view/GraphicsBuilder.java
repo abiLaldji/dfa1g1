22c98f7ea974bf198219f046d72a14391f98e82d
jeremydussaux <jeremy.dussaux@viacesi.fr>
2018-04-25 08:41:24 +0200

    phase 3

---
+++ src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -0,0 +1,35 @@
+package jpu2016.dogfight.view;
+import java.awt.image.*;
+import jpu2016.dogfight.model.*;
+
+
+public class GraphicsBuilder {
+	
+	private BufferedImage emptySky = new BufferedImage();
+	private IDogFightModel dogfightModel = new IDogfightModel();
+	
+	public GraphicsBuilder(IDogfightModel dogfightModel) {
+		
+	}
+	
+	public void apllyModelToGraphic(Graphics graphics , ImageObserver observer) {	
+		
+	}
+	
+	private void buildEmptySky() {
+		
+	}
+	
+	private void drawMobile(IMobile mobile, Graphics graphics, ImageObserver observer ) {
+		
+	}
+	
+	public int getGlobalWidth() {
+		return 1;
+	}
+	public int getGlobalHeight() {
+		return 1;
+	}
+	
+	
+}
